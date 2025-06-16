b356cc3931a76d2d8cc941830b4410a4c5da1692
Laís Sampaio <laissampaio308@gmail.com>
2025-06-16 20:03:17 -0300

    Fixing sign in button

--- src/components/menu/Navbar.tsx
+++ src/components/menu/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar(){
             <h1>Dispensa do Bem</h1>
             <div className="links">
                 <div className="carrinho"><img src="./carrinho-carrinho.png" alt="" /></div>
-                <Link to="/cadastro" className="cadastro">Cadastrar-se</Link>  
+                <Link to="/cadastro" className="cadastro-btn">Cadastrar-se</Link>  
             </div>
         </div>
 
